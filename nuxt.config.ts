68165de521e0e0584d0ece9a87245f8eedfe4b7d
Rajeevn Majhi <rajeevkumarmajhi@gmail.com>
2023-09-25 21:04:36 +0545

    --> Linked bootstrap --> Setup config for global links and title and meta tags --> Created Pages Home, About Page, Contact Page and added bootstrap navbar to navigate them

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title : 'Next 3 CRUD Application',
+      meta : [
+        { 'charset' : 'utf-8' },
+        { 'name': 'viewport', content:'width=device-width, initial-scale=1' },
+      ],
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Roboto&display=swap',
+          crossorigin: ''
+        },
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/public/favicon.ico', 
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css',
+        }
+      ],
+      script:[
+        {
+          src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',
+          type:'text/javascript',
+        }
+      ]
+    }
+  },
   devtools: { enabled: true }
 })
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>I am a Home Page</h1>
+    </div>
+</template>
+<script setup lang="ts">
+useHead({
+    title: 'My App',
+});
+</script>
